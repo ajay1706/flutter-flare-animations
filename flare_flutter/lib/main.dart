fe553b3fc327b8e256e5ef3badc7a94121d6bb5b
ajay1706 <ajay97456@gmail.com>
2019-09-24 12:54:59 +0530

    adding file & code & flare file

---
+++ flare_flutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+void main() => runApp(MaterialApp( 
+  home: HomePage(),
+));
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      
+    );
+  }
+}
